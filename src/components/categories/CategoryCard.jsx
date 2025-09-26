c22ede3ac47865972759e7c781ec1b7c1cf775bd
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2025-09-26 18:19:25 -0500

    delete buttons in categoryCard and VisitsList implemented, and Action column added to VisitList.jsx

--- src/components/categories/CategoryCard.jsx
+++ src/components/categories/CategoryCard.jsx
@@ -2,10 +2,12 @@ import { useEffect } from "react";
 import { Navigate, useNavigate } from "react-router";
 import api from "../../api/user.api";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 const CategoryCard = ({ category, surveysession_id, visit_id }) => {
   const navigate = useNavigate();
   const [isCompleted,setIsCompleted]=useState(false);
+  const [isEliminated,setIsEliminated]=useState(false);
 
   useEffect(()=>{
 
@@ -13,9 +15,9 @@ const CategoryCard = ({ category, surveysession_id, visit_id }) => {
       try {
       const res=await api.get(`category/category_completed?category_id=${category.id}&visit_id=${visit_id}`)
       console.log('respuesta de category card', res.data.is_completed)
-      if (res.data.is_completed){
-        setIsCompleted(res.data.is_completed)
-      }
+      
+      setIsCompleted(res.data.is_completed)
+      
       
     } catch (error) {
       console.log('error in category_completed fetch',error)     
@@ -25,7 +27,21 @@ const CategoryCard = ({ category, surveysession_id, visit_id }) => {
 
     get_category_is_completed();
     
-  },[])
+  },[isEliminated])
+
+  const handleDeleteForm= async (category_id)=>{
+    try {
+
+      const resp=await api.delete(`response/delete_responses_by_category/?category_id=${category_id}&visit_id=${visit_id}`)
+      setIsEliminated(prev=>!prev)
+      toast.success('responses of the form deleted successfully')
+      
+    } catch (error) {
+
+      console.log('error in handleDeleteForm from CategoryCard.jsx',error)
+      
+    }
+  }
 
   return (
     <div
@@ -40,17 +56,34 @@ const CategoryCard = ({ category, surveysession_id, visit_id }) => {
         />
       )}
       <h1 className="font-bold uppercase text-black">{category.name}</h1>
+      {!isCompleted?
       <button
         
-        disabled={isCompleted}
+        
         onClick={() => {
           navigate(`form/${category.id}`);
         }}
         type="button"
-        class={`focus:outline-none text-white ${!isCompleted? 'bg-green-700 hover:bg-green-800' :'bg-red-700'}  my-8  focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800`}
+        class={`focus:outline-none text-white bg-green-700 hover:bg-green-800  my-8  focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800`}
       >
-        {isCompleted? 'Completado':'Iniciar'}
+        Iniciar
       </button>
+      :
+
+      <button
+        
+        
+        onClick={() => {
+          handleDeleteForm(category.id)
+        }}
+        type="button"
+        class={`focus:outline-none text-white bg-red-700 hover:bg-red-800  my-8  focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800`}
+      >
+        Eliminar Envio
+      </button>
+
+      }
+      
     </div>
   );
 };
--- src/components/visits/VisitsList.jsx
+++ src/components/visits/VisitsList.jsx
@@ -5,66 +5,54 @@ import api from "../../api/user.api";
 import { useNavigate } from "react-router";
 import toast from "react-hot-toast";
 
-const VisitsList = ({surveysession_id}) => {
+const VisitsList = ({ surveysession_id }) => {
+  const [visits, setVisits] = useState([]);
 
-    const [visits,setVisits]=useState([])
-    
-    const [loading,setLoading]=useState(true);
+  const [loading, setLoading] = useState(true);
 
-    const {userLogged}=useAuthStore();
+  const { userLogged } = useAuthStore();
 
-    const [visitedIsDeleted,setVisitIsDeleted]=useState(false)
+  const [visitedIsDeleted, setVisitIsDeleted] = useState(false);
 
-    const navigate=useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        async function getVisits(){
-
-          try {
-            const res= await api.get(`visit/sessionvisits?surveysession_id=${surveysession_id}`)
-            console.log('esta es la respuesta en visit',res)
-            setVisits(res.data)
-            
-             
-        }
-        catch (error){
-            console.log('error in SessionList',error)
-        }
-        finally{
-          setLoading(false)
-        }
-        }
-        getVisits();
-    },[userLogged,visitedIsDeleted])
-
-    
-
-    if (loading){
-      return <div className="text-center p-10 text-white">Loading users...</div>;
-    }
-
-    const handleClickStart= (visit_id)=>{
-       console.log('este es el surveysession_id',surveysession_id)
-        navigate(`categories/${visit_id}`)
-    }
-
-    const handleClickDelete=async (visit_id)=>{
+  useEffect(() => {
+    async function getVisits() {
       try {
-
-        const res=await api.delete(`visit/delete/${visit_id}`)
-
-        setVisitIsDeleted(prev=>!prev)
-
-        toast.success('Visita Eliminada Exitosamente')
-
-        
-        
+        const res = await api.get(
+          `visit/sessionvisits?surveysession_id=${surveysession_id}`
+        );
+        console.log("esta es la respuesta en visit", res);
+        setVisits(res.data);
       } catch (error) {
-
-        console.log('error deleting visit',error)
-        
+        console.log("error in SessionList", error);
+      } finally {
+        setLoading(false);
       }
     }
+    getVisits();
+  }, [userLogged, visitedIsDeleted]);
+
+  if (loading) {
+    return <div className="text-center p-10 text-white">Loading users...</div>;
+  }
+
+  const handleClickStart = (visit_id) => {
+    console.log("este es el surveysession_id", surveysession_id);
+    navigate(`categories/${visit_id}`);
+  };
+
+  const handleClickDelete = async (visit_id) => {
+    try {
+      const res = await api.delete(`visit/delete/${visit_id}`);
+
+      setVisitIsDeleted(prev => !prev);
+
+      toast.success("Visita Eliminada Exitosamente");
+    } catch (error) {
+      console.log("error deleting visit", error);
+    }
+  };
 
   return (
     <div className="container mx-auto p-4 sm:p-6 lg:p-8">
@@ -76,22 +64,45 @@ const VisitsList = ({surveysession_id}) => {
         <table className="w-full text-sm text-left text-gray-500">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
-              <th scope="col" className="py-3 px-6">ID</th>
-              <th scope="col" className="py-3 px-6">Numero de Sesion</th>
-              <th scope="col" className="py-3 px-6">Numemro de Visita</th>
-              <th scope="col" className="py-3 px-6">Fecha de Visita</th>
-              <th scope="col" className="py-3 px-6">Hora de Inicio</th>
-              <th scope="col" className="py-3 px-6">Hora de Finalizacion</th>
-              <th scope="col" className="py-3 px-6">Estado</th>
+              <th scope="col" className="py-3 px-6">
+                ID
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Numero de Sesion
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Numemro de Visita
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Fecha de Visita
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Hora de Inicio
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Hora de Finalizacion
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Estado
+              </th>
+              <th>
+                Accion
+              </th>
             </tr>
           </thead>
           <tbody>
-            {visits.map((visit) => (
-              <tr 
+            {visits.map((visit) =>{   
               
-              key={visit.id} className="bg-white border-b hover:bg-gray-100">
+              return (
+              <tr
+                key={visit.id}
+                className="bg-white border-b hover:bg-gray-100"
+              >
                 {/* ID is bolded to make it stand out */}
-                <th   scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
+                <th
+                  scope="row"
+                  className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap"
+                >
                   {visit.id}
                 </th>
                 <td className="py-4 px-6">{visit.surveysession}</td>
@@ -99,34 +110,44 @@ const VisitsList = ({surveysession_id}) => {
                 <td className="py-4 px-6">{visit.visit_date}</td>
                 <td className="py-4 px-6">{visit.start_time}</td>
                 <td className="py-4 px-6">{visit.end_time}</td>
-                <td className="py-4 px-6">{visit.complete? "Completada":
+                <td className="py-4 px-6">{visit.complete?
+                <h1>
+                  Completa
+                </h1>
+                :
+                <h1>
+                  Incompleta
+                </h1>
+              }</td>
                 
-                <div>
-                  <button
-                
-                type="button" 
-                class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                onClick={()=>handleClickStart(visit.id)}
-                >Iniciar</button>
+                <td className="py-4 px-6">
+                  {
+                    <div  className="flex items items-center justify-center">
+                      <button
+                        type="button"
+                        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                        onClick={() => handleClickStart(visit.id)}
+                      >
+                        Gestionar
+                      </button>
 
-                <button
-                
-                type="button" 
-                class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-                onClick={()=>handleClickDelete(visit.id)}
-                >Eliminar</button>
-                </div>
-                  
-                
-}</td>
+                      <button
+                        type="button"
+                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                        onClick={() => handleClickDelete(visit.id)}
+                      >
+                        Eliminar
+                      </button>
+                    </div>
+                  }
+                </td>
               </tr>
-            ))}
+            )})}
           </tbody>
         </table>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
 export default VisitsList;
