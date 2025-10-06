a5b82e972915ba8994c5b5eab0af98c2c3bf1c59
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2025-10-06 17:12:23 -0500

    inputs and fields deleted in CreateVisit.jsx file, because only visit numever is needed for the moment

--- src/components/visits/CreateVisit.jsx
+++ src/components/visits/CreateVisit.jsx
@@ -18,18 +18,18 @@ const CreateVisit = ({ surveysession_id }) => {
       ? {
           surveysession: surveysession_id,
           visit_number: visit.visit_number,
-          visit_date: visit.visit_date,
-          start_time: visit.start_time,
-          end_time: visit.end_time,
-          complete: visit.complete,
+          // visit_date: visit.visit_date,
+          // start_time: visit.start_time,
+          // end_time: visit.end_time,
+          // state: visit.state,
         }
       : {
           surveysession: surveysession_id,
           visit_number: "",
-          visit_date: "",
-          start_time: "",
-          end_time: "",
-          complete: false,
+          // visit_date: "",
+          // start_time: "",
+          // end_time: "",
+          // state: 0,
         }
   );
 
@@ -42,6 +42,7 @@ const CreateVisit = ({ surveysession_id }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log('start Time',formData.start_time)
     try {
       setLoading(true);
       
@@ -120,7 +121,7 @@ const CreateVisit = ({ surveysession_id }) => {
         </div>
 
         {/* Fecha de visita */}
-        <div>
+        {/* <div>
           <label htmlFor="visit_date" className="block text-sm font-medium text-slate-700 mb-1">
             Fecha de Visita
           </label>
@@ -132,10 +133,10 @@ const CreateVisit = ({ surveysession_id }) => {
             className="block w-full px-3 py-2 bg-slate-50 border border-slate-300 rounded-md text-slate-900 placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
             onChange={handleChange}
           />
-        </div>
+        </div> */}
 
         {/* Hora de Inicio */}
-        <div>
+        {/* <div>
           <label htmlFor="start_time" className="block text-sm font-medium text-slate-700 mb-1">
             Hora de Inicio
           </label>
@@ -147,10 +148,10 @@ const CreateVisit = ({ surveysession_id }) => {
             className="block w-full px-3 py-2 bg-slate-50 border border-slate-300 rounded-md text-slate-900 placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
             onChange={handleChange}
           />
-        </div>
+        </div> */}
 
         {/* Hora de Finalizacion */}
-        <div>
+        {/* <div>
           <label htmlFor="end_time" className="block text-sm font-medium text-slate-700 mb-1">
             Hora de Finalización
           </label>
@@ -162,7 +163,7 @@ const CreateVisit = ({ surveysession_id }) => {
             className="block w-full px-3 py-2 bg-slate-50 border border-slate-300 rounded-md text-slate-900 placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
             onChange={handleChange}
           />
-        </div>
+        </div> */}
       </div>
 
       {/* Submit Button */}
