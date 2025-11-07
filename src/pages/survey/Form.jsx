f206fa2b8b0aa7d926468e4de904da1954b04230
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2025-11-07 14:35:17 -0500

    red * added to indicate that the question is required

--- src/pages/survey/Form.jsx
+++ src/pages/survey/Form.jsx
@@ -201,9 +201,18 @@ const Form = () => {
                     className="pb-8 mb-8 border-b border-gray-200 last:border-b-0 last:mb-0 last:pb-0 space-y-3"
                   >
                     {/* Styled question text */}
-                    <legend className="text-xl font-semibold text-gray-800 mb-4">
-                      {`${q.code} ${q.description}`}
-                    </legend>
+
+                    <div className="flex gap-3">
+                      {q.is_required && (
+                        <legend className="text-red-700 font-bold text-lg">
+                          *
+                        </legend>
+                      )}
+
+                      <legend className="text-xl font-semibold text-gray-800 mb-4">
+                        {`${q.code} ${q.description}`}
+                      </legend>
+                    </div>
 
                     {/* Options map */}
                     {q.options.map((option) => (
@@ -286,9 +295,16 @@ const Form = () => {
                     key={q.id}
                     className="pb-8 mb-8 border-b border-gray-200 last:border-b-0 last:mb-0 last:pb-0"
                   >
-                    <legend className="text-xl font-semibold text-gray-800 mb-4">
-                      {`${q.code} ${q.description}`}
-                    </legend>
+                    <div className="flex gap-3">
+                      {q.is_required && (
+                        <legend className="text-red-700 font-bold text-lg">
+                          *
+                        </legend>
+                      )}
+                      <legend className="text-xl font-semibold text-gray-800 mb-4">
+                        {`${q.code} ${q.description}`}
+                      </legend>
+                    </div>
 
                     {/* Sub-questions map */}
                     {q.sub_questions.map((sub_q) => {
